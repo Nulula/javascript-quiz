7947244378cf99aed3cecd59aed84a03f5d99d31
Nulula <nulula@gmail.com>
2023-01-13 19:21:26 +0000

    initial files

---
+++ assets/js/questions.js
@@ -0,0 +1,45 @@
+let quizQuestions = [
+    {
+      question: "Which built-in method calls a function for each element in the array?",
+      choices: {
+        1: "while()",
+        2: "loop()",
+        3: "forEach()",
+        4: "None of the above",
+      },
+      correctChoice: "3"
+    },
+  
+    {
+      question: "Which of the following function of Array object returns a new array comprised of this array joined with other array(s) and/or value(s)?",
+      choices: {
+        1: "concat()",
+        2: "pop()",
+        3: "push()",
+        4: "match()",
+      },
+      correctChoice: "1"
+    },
+  
+    {
+      question: "JavaScript file has an extension of:",
+      choices: {
+        1: ".xml",
+        2: ".js",
+        3: ".react",
+        4: ".sc",
+      },
+      correctChoice: "2"
+    },
+  
+    {
+      question: "Which of the dialog box display a message and a data entry field?",
+      choices: {
+        1: "alert()",
+        2: "msg()",
+        3: "confirm()",
+        4: "prompt()",
+      },
+      correctChoice: "4"
+    },
+  ];
