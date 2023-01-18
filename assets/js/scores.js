a62459c89cc472be967f6edc9ee9ea75a0de63b2
Nulula <nulula@gmail.com>
2023-01-18 01:32:38 +0000

    scoring fixed, README done

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,11 +1,30 @@
-//it will be a similar function to build a list of highest scores and initials
+let score = JSON.parse(localStorage.getItem("score"));
+let initials = JSON.parse(localStorage.getItem("initials"));
 
-let scoreStr = JSON.parse(localStorage.getItem("score"));
-let initialsStr = JSON.parse(localStorage.getItem("initials"));
+if(localStorage.getItem("scores") === null) {
+    localStorage.setItem("scores", JSON.stringify([]));
+}
+let scores = JSON.parse(localStorage.getItem("scores"));
 
-var scoreCount = [];
-scoreCount.push(scoreStr,initialsStr);
+scores.push({
+    initials: initials,
+    score: score
+});
 
+scores.sort((a,b) => b.score - a.score);
+localStorage.setItem("scores", JSON.stringify(scores));
 
+var olEl = document.querySelector("#highscores");
 
+for (let i=0;i<scores.length;i++) {
+    var liEl = document.createElement("li");
+    liEl.textContent = String(scores[i].initials) + ": " + scores[i].score;
+    olEl.appendChild(liEl);
+}
 
+var buttonClear = document.querySelector("#clear");
+
+buttonClear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+})
